08d1176e2fa22d7d59b59671c14f2a3e6f78732a
Fabricio Baixo <fabriciobaixo@gmail.com>
2019-08-16 01:48:39 -0300

    Removendo pessoa no banco de dados

--- src/aplicacao/Programa.java
+++ src/aplicacao/Programa.java
@@ -1,3 +1,9 @@
+/*
+ * Caso esteja usando Java 9 para que o hibernate não acuse problema com jabx
+ * adicionar no VM arguments (Run As -> Run Configurations -> Arguments)
+ * o seguinte modulo: --add-modules java.xml.bind -->
+ */
+
 package aplicacao;
 
 import javax.persistence.EntityManager;
@@ -9,7 +15,6 @@ import dominio.Pessoa;
 public class Programa {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		
 		
 		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
@@ -17,6 +22,12 @@ public class Programa {
 		
 		//Buscando pessoa no banco de dados
 		Pessoa p = em.find(Pessoa.class,2);
+		
+		//Sempre que uma operacao nao for uma consulta se deve iniciar uma transacao
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
 		
 		System.out.println(p);
 		
